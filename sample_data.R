2f5a6653d2e84c238efbd59ae2e7d0582f073f06
進撃の雯雯老師 <askahu6@gmail.com>
2017-06-08 15:10:02 -0500

    Create sample_data.R

--- sample_data.R
+++ sample_data.R
@@ -1,1 +1,16 @@
+############################
+# Sample data
+############################
+
+Y <- scale(data.matrix(iris[,1:4]))
+colnames(Y) <- paste0('y',1:4)
+X <- scale(qpca(sapply(1:ncol(Y),function(i){lm(Y[,i]~Y[,-i])$residual}),lambda=.2)$X)
+colnames(X) <- paste0('x',1:3)
+rawi <- cbind(Y,X)
+
+raw <- lapply(1:3,function(i){
+  set.seed(i)
+  rawi <- rawi + rnorm(length(rawi),0,0.3)
+  rawi
+})
 
