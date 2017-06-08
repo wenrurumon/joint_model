fb93a159197d25553f5df8571a6cddcc8fcd3203
進撃の雯雯老師 <askahu6@gmail.com>
2017-06-08 15:10:28 -0500

    Create macro.R

---
+++ macro.R
@@ -0,0 +1,25 @@
+############################
+# Macro
+############################
+
+lasso <- function(Y,X,lambda){
+  slimi <- flare::slim(X=X,Y=Y,lambda=lambda,rho=1,method='lasso',verbose=FALSE)
+  Xsel <- (slimi$beta!=0)
+  X <- X[,Xsel,drop=F]
+  list(Xsel=Xsel,lm=lm(Y~X))
+}
+
+qpca <- function(A,lambda=0){
+  A.svd <- svd(A)
+  d <- A.svd$d-lambda*A.svd$d[1]
+  d <- d[d > 1e-8]
+  r <- length(d)
+  prop <- d^2; info <- sum(prop)/sum(A.svd$d^2);prop <- cumsum(prop/sum(prop))
+  d <- diag(d,length(d),length(d))
+  u <- A.svd$u[,1:r,drop=F]
+  v <- A.svd$v[,1:r,drop=F]
+  x <- u%*%sqrt(d)
+  y <- sqrt(d)%*%t(v)
+  z <- x %*% y
+  list(rank=r,X=x,Y=y,Z=x%*%y,prop=prop,info=info)
+}
