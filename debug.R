5988a06a2dea6a2f5ffa934f81851a5c96b3de1b
進撃の雯雯老師 <askahu6@gmail.com>
2017-06-08 17:39:27 -0500

    Update debug.R

--- debug.R
+++ debug.R
@@ -64,7 +64,7 @@ raw <- lapply(1:3,function(i){
 })
 
 ############################
-# Algorithm
+# Initialization and Interation 1
 ############################
 
 #Global Config
@@ -132,5 +132,41 @@ U.check <- pn(do.call(cbind,U1) - do.call(cbind,U0))
 if(any(c(D.check,Z.check,U.check) < 0.0001)){
   print('converge')
 } else {
+  print(c(D.check,Z.check,U.check))
+  D0 <- D1; Z0 <- Z1; U0 <- U1
+}
+
+############################
+# Interation 1+
+############################
+
+D1 <- lapply(1:L,function(l){
+  Yl <- YX[[l]]$Y
+  Xl <- YX[[l]]$X
+  Wi <- cbind(Yl[,-i,drop=F],Xl)
+  yi <- Yl[,i,drop=F]
+  d1 <- (ginv(t(Wi)%*%Xl%*%ginv(t(Xl)%*%Xl)%*%t(Xl)%*%Wi + rho)%*%
+           t(Wi)%*%Xl%*%ginv(t(Xl)%*%Xl)%*%t(Xl)%*%yi + 
+           rho * (Z0[[l]] - U0[[l]]))
+  d1
+})
+Z1 <- lapply(1:L,function(l){
+  Zr1 <- positive(1 - lambda * sqrt(L) / (rho * pn(D1[[l]][1:(J-1)] - U0[[l]][1:(J-1)]))) * (D1[[l]] - U0[[l]])
+  Zr1 <- Zr1[1:(J-1)]
+  Zb1 <- positive(1 - lambda * sqrt(L) / (rho * pn(D1[[l]][-(1:(J-1))] - U0[[l]][-(1:(J-1))]))) * (D1[[l]] - U0[[l]])
+  Zb1 <- Zb1[-(1:(J-1))]
+  Z1 <- as.matrix(c(Zr1,Zb1))
+  Z1
+})
+U1 <- lapply(1:L,function(l){
+  U0[[l]] + D1[[l]] - Z1[[l]]
+})
+D.check <- pn(do.call(cbind,D1) - do.call(cbind,D0))
+Z.check <- pn(do.call(cbind,Z1) - do.call(cbind,Z0))
+U.check <- pn(do.call(cbind,U1) - do.call(cbind,U0))
+if(any(c(D.check,Z.check,U.check) < 0.0001)){
+  print('converge')
+} else {
+  print(c(D.check,Z.check,U.check))
   D0 <- D1; Z0 <- Z1; U0 <- U1
 }
