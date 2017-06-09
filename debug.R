2402390cd2366646f4a2d1a32afb424e72380bcc
進撃の雯雯老師 <askahu6@gmail.com>
2017-06-08 23:01:15 -0500

    Update debug.R

--- debug.R
+++ debug.R
@@ -70,7 +70,7 @@ raw <- lapply(1:3,function(i){
 #Global Config
 YX <- raw
 lambda <- .1
-rho <- 1
+rho <- 10
 L <- length(YX)
 J <- ncol(raw[[1]]$Y)
 K <- ncol(raw[[1]]$X)
@@ -109,64 +109,12 @@ D1 <- lapply(1:L,function(l){
 
 #interation: Z(m+1)
 
-Z1 <- lapply(1:L,function(l){
-  Zr1 <- positive(1 - lambda * sqrt(L) / (rho * pn(D1[[l]][1:(J-1)] - U0[[l]][1:(J-1)]))) * (D1[[l]] - U0[[l]])
-  Zr1 <- Zr1[1:(J-1)]
-  Zb1 <- positive(1 - lambda * sqrt(L) / (rho * pn(D1[[l]][-(1:(J-1))] - U0[[l]][-(1:(J-1))]))) * (D1[[l]] - U0[[l]])
-  Zb1 <- Zb1[-(1:(J-1))]
-  Z1 <- as.matrix(c(Zr1,Zb1))
-  Z1
-})
+D0m <- do.call(cbind,D0)
+U0m <- do.call(cbind,U0)
+Z1 <- positive(1 - lambda * sqrt(L) / (rho * (rowMeans(D0m - U0m)^2))) * (D0m-U0m)
+Z1 <- lapply(1:L,function(l){Z1[,l,drop=F]})
+U1 <- lapply(1:L,function(l){U0[[l]] + D1[[l]] - Z1[[l]]})
 
-#interation: U(m+1)
 
-U1 <- lapply(1:L,function(l){
-  U0[[l]] + D1[[l]] - Z1[[l]]
-})
 
-#check convergence
 
-D.check <- pn(do.call(cbind,D1) - do.call(cbind,D0))
-Z.check <- pn(do.call(cbind,Z1) - do.call(cbind,Z0))
-U.check <- pn(do.call(cbind,U1) - do.call(cbind,U0))
-if(any(c(D.check,Z.check,U.check) < 0.0001)){
-  print('converge')
-} else {
-  print(c(D.check,Z.check,U.check))
-  D0 <- D1; Z0 <- Z1; U0 <- U1
-}
-
-############################
-# Interation 1+
-############################
-
-D1 <- lapply(1:L,function(l){
-  Yl <- YX[[l]]$Y
-  Xl <- YX[[l]]$X
-  Wi <- cbind(Yl[,-i,drop=F],Xl)
-  yi <- Yl[,i,drop=F]
-  d1 <- (ginv(t(Wi)%*%Xl%*%ginv(t(Xl)%*%Xl)%*%t(Xl)%*%Wi + rho)%*%
-           t(Wi)%*%Xl%*%ginv(t(Xl)%*%Xl)%*%t(Xl)%*%yi + 
-           rho * (Z0[[l]] - U0[[l]]))
-  d1
-})
-Z1 <- lapply(1:L,function(l){
-  Zr1 <- positive(1 - lambda * sqrt(L) / (rho * pn(D1[[l]][1:(J-1)] - U0[[l]][1:(J-1)]))) * (D1[[l]] - U0[[l]])
-  Zr1 <- Zr1[1:(J-1)]
-  Zb1 <- positive(1 - lambda * sqrt(L) / (rho * pn(D1[[l]][-(1:(J-1))] - U0[[l]][-(1:(J-1))]))) * (D1[[l]] - U0[[l]])
-  Zb1 <- Zb1[-(1:(J-1))]
-  Z1 <- as.matrix(c(Zr1,Zb1))
-  Z1
-})
-U1 <- lapply(1:L,function(l){
-  U0[[l]] + D1[[l]] - Z1[[l]]
-})
-D.check <- pn(do.call(cbind,D1) - do.call(cbind,D0))
-Z.check <- pn(do.call(cbind,Z1) - do.call(cbind,Z0))
-U.check <- pn(do.call(cbind,U1) - do.call(cbind,U0))
-if(any(c(D.check,Z.check,U.check) < 0.0001)){
-  print('converge')
-} else {
-  print(c(D.check,Z.check,U.check))
-  D0 <- D1; Z0 <- Z1; U0 <- U1
-}
