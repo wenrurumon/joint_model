72c1ffa434f1925cfd1159b943625c3ae468cb92
進撃の雯雯老師 <askahu6@gmail.com>
2017-06-15 16:10:20 -0500

    Create main.R

--- main.R
+++ main.R
@@ -1,108 +1,78 @@
+#2 stage ols
 
-############################
-# Initialization and Interation 1
-############################
-
-#Global Config
-YX <- raw
-lambda <- .1
-rho <- 1
-L <- length(YX)
-J <- ncol(raw[[1]]$Y)
-K <- ncol(raw[[1]]$X)
-
-#for i
-i <- 1
-
-#init: DELTAi(0), Zi(0), U0
-D0 <- lapply(1:L,function(l){
-  Yl <- YX[[l]]$Y
-  Xl <- YX[[l]]$X
-  Wi <- cbind(Yl[,-i,drop=F],Xl)
-  yi <- Yl[,i,drop=F]
-  u0 <- 0
-  di0 <- (ginv(t(Wi)%*%Xl%*%ginv(t(Xl)%*%Xl)%*%t(Xl)%*%Wi + rho)%*%
-            t(Wi)%*%Xl%*%ginv(t(Xl)%*%Xl)%*%t(Xl)%*%yi)
-  di0
-})
-Z0 <- D0
-U0 <- lapply(1:L,function(l){
-  D0[[l]]-Z0[[l]]
-})
-
-#interation: DELTAi(m)
-
-D1 <- lapply(1:L,function(l){
-  Yl <- YX[[l]]$Y
-  Xl <- YX[[l]]$X
-  Wi <- cbind(Yl[,-i,drop=F],Xl)
-  yi <- Yl[,i,drop=F]
-  d1 <- (ginv(t(Wi)%*%Xl%*%ginv(t(Xl)%*%Xl)%*%t(Xl)%*%Wi + rho)%*%
-           t(Wi)%*%Xl%*%ginv(t(Xl)%*%Xl)%*%t(Xl)%*%yi + 
-           rho * (Z0[[l]] - U0[[l]]))
-  d1
-})
-
-#interation: Z(m+1)
-
-Z1 <- lapply(1:L,function(l){
-  Zr1 <- positive(1 - lambda * sqrt(L) / (rho * pn(D1[[l]][1:(J-1)] - U0[[l]][1:(J-1)]))) * (D1[[l]] - U0[[l]])
-  Zr1 <- Zr1[1:(J-1)]
-  Zb1 <- positive(1 - lambda * sqrt(L) / (rho * pn(D1[[l]][-(1:(J-1))] - U0[[l]][-(1:(J-1))]))) * (D1[[l]] - U0[[l]])
-  Zb1 <- Zb1[-(1:(J-1))]
-  Z1 <- as.matrix(c(Zr1,Zb1))
-  Z1
-})
-
-#interation: U(m+1)
-
-U1 <- lapply(1:L,function(l){
-  U0[[l]] + D1[[l]] - Z1[[l]]
-})
-
-#check convergence
-
-D.check <- pn(do.call(cbind,D1) - do.call(cbind,D0))
-Z.check <- pn(do.call(cbind,Z1) - do.call(cbind,Z0))
-U.check <- pn(do.call(cbind,U1) - do.call(cbind,U0))
-if(any(c(D.check,Z.check,U.check) < 0.0001)){
-  print('converge')
-} else {
-  print(c(D.check,Z.check,U.check))
-  D0 <- D1; Z0 <- Z1; U0 <- U1
+calc_d <- function(wi,x,yi,rho=0,Z=0,U=0){
+  if(sum(abs(Z),abs(U))==0){
+    Z <- 0
+  } else {
+    Z <- Z-U
+  }
+  out <- ginv(t(wi)%*%x%*%ginv(t(x)%*%x)%*%t(x)%*%wi+rho*diag(ncol(wi))) %*%
+    (t(wi)%*%x%*%ginv(t(x)%*%x)%*%t(x)%*%yi+rho*Z)
+  rownames(out) <- colnames(wi)
+  out
 }
 
-############################
-# Interation 1+
-############################
+#sparse coef given Y, X, i within l
+equali <- function(Y,X,i,lambda1=0.5,rho=1,wsel=NULL,Z=0,U=0){
+  yi <- Y[,i,drop=F]
+  x <- X
+  w <- cbind(Y[,-i],x)
+  if(is.null(wsel)){
+    wsel <- selbycor(yi,w,ncol(x)*lambda1)  
+  }
+  wi <- w[,wsel,drop=F]
+  di <- calc_d(wi,x,yi,rho,Z,U)
+  wsel[wsel] <- di
+  wsel
+}
 
-D1 <- lapply(1:L,function(l){
-  Yl <- YX[[l]]$Y
-  Xl <- YX[[l]]$X
-  Wi <- cbind(Yl[,-i,drop=F],Xl)
-  yi <- Yl[,i,drop=F]
-  d1 <- (ginv(t(Wi)%*%Xl%*%ginv(t(Xl)%*%Xl)%*%t(Xl)%*%Wi + rho)%*%
-           t(Wi)%*%Xl%*%ginv(t(Xl)%*%Xl)%*%t(Xl)%*%yi + 
-           rho * (Z0[[l]] - U0[[l]]))
-  d1
-})
-Z1 <- lapply(1:L,function(l){
-  Zr1 <- positive(1 - lambda * sqrt(L) / (rho * pn(D1[[l]][1:(J-1)] - U0[[l]][1:(J-1)]))) * (D1[[l]] - U0[[l]])
-  Zr1 <- Zr1[1:(J-1)]
-  Zb1 <- positive(1 - lambda * sqrt(L) / (rho * pn(D1[[l]][-(1:(J-1))] - U0[[l]][-(1:(J-1))]))) * (D1[[l]] - U0[[l]])
-  Zb1 <- Zb1[-(1:(J-1))]
-  Z1 <- as.matrix(c(Zr1,Zb1))
-  Z1
-})
-U1 <- lapply(1:L,function(l){
-  U0[[l]] + D1[[l]] - Z1[[l]]
-})
-D.check <- pn(do.call(cbind,D1) - do.call(cbind,D0))
-Z.check <- pn(do.call(cbind,Z1) - do.call(cbind,Z0))
-U.check <- pn(do.call(cbind,U1) - do.call(cbind,U0))
-if(any(c(D.check,Z.check,U.check) < 0.0001)){
-  print('converge')
-} else {
-  print(c(D.check,Z.check,U.check))
-  D0 <- D1; Z0 <- Z1; U0 <- U1
+#sparse coef given Y,X, i cross L
+maini <- function(raw,i=1,rho=1,lambda1=.7,lambda2=.1,a=.3,itnmax=100){
+  #config_in
+  L <- length(raw)
+  M <- ncol(raw[[1]]$Y)
+  J <- ncol(raw[[1]]$X)
+  out <- matrix(0,M+J,L,dimnames=list(c(colnames(raw[[1]]$Y),colnames(raw[[1]]$X)),1:L))
+  #init0
+  d0 <- sapply(1:L,function(l){
+    Yl <- raw[[l]]$Y
+    Xl <- raw[[l]]$X
+    equali(Yl,Xl,i,lambda1,NULL,rho=rho)
+  })
+  z0 <- d0
+  wsel <- rowSums(z0!=0)>0
+  u0 <- d0-z0
+  d1 <- d0
+  #initm
+  itn <- 0
+  while(TRUE){
+    itn <- itn+1
+    if(itn>itnmax){break}
+    rho <- rho * a
+    lambda2 <- lambda2 * a
+    d1 <- sapply(1:L,function(l){
+      Yl <- raw[[l]]$Y
+      Xl <- raw[[l]]$X
+      equali(Y=Yl,X=Xl,i=i,lambda1=NULL,rho=rho,wsel=wsel,Z=z0[wsel,l],U=u0[wsel,l])
+    })
+    if(pn(d1-d0)<=1e-8){break}
+    z1 <- positive(1-sqrt(L)*lambda2/rho/apply(d1-u0,1,pn)) * (d1-u0)
+    z1[is.na(z1)] <- 0
+    wsel <- rowSums(z1!=0)>0
+    if(sum(wsel)==0){
+      d1[,] <- 0
+      break
+    }
+    u1 <- u0 + d1 - z1
+    d0 <- d1; z0 <- z1; u0 <- u1
+  }
+  #result
+  out[-i,] <- d1
+  out
+}
+
+#full network 
+main <- function(raw,rho=1,lambda1=.7,lambda2=.1,a=.3,itnmax=100){
+  out <- lapply(1:ncol(raw[[1]]$Y),maini,raw=raw)
+  abind(out,along=0)
 }
