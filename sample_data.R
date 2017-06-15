cba9cb38101cf9fb36889bc5947600461374f7b4
進撃の雯雯老師 <askahu6@gmail.com>
2017-06-15 15:21:34 -0500

    Create sample_data.R

--- sample_data.R
+++ sample_data.R
@@ -2,14 +2,17 @@
 # Sample data
 ############################
 
-Y <- scale(data.matrix(iris[,1:4]))
-colnames(Y) <- paste0('y',1:4)
-X <- scale(qpca(sapply(1:ncol(Y),function(i){lm(Y[,i]~Y[,-i])$residual}),lambda=.2)$X)
-colnames(X) <- paste0('x',1:3)
-rawi <- cbind(Y,X)
+Y <- sapply(1:5,function(x) rnorm(1000))
+colnames(Y) <- paste0('y',1:5)
+X <- sapply(1:10,function(x) rnorm(1000))
+colnames(X) <- paste0('x',1:10)
+Y[,1] <- Y[,3] + Y[,5] + X[,2] + X[,3]
+Y[,2] <- Y[,3] + Y[,4] + X[,5] + X[,9]
+Y <- scale(Y)
+X <- scale(X)
 
-raw <- lapply(1:3,function(i){
-  set.seed(i)
-  rawi <- rawi + rnorm(length(rawi),0,0.3)
-  list(Y=scale(rawi)[,1:4,drop=F],X=scale(rawi)[,-1:-4,drop=F])
+raw <- lapply(1:3,function(l){
+  Y <- dummy(Y,.1)
+  X <- dummy(X,.3)
+  list(Y=Y,X=X)
 })
