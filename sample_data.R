ff6bd350d8e9a1eba943767c267bf27897463aa0
進撃の雯雯老師 <askahu6@gmail.com>
2017-06-15 16:10:34 -0500

    Update sample_data.R

--- sample_data.R
+++ sample_data.R
@@ -6,8 +6,8 @@ Y <- sapply(1:5,function(x) rnorm(1000))
 colnames(Y) <- paste0('y',1:5)
 X <- sapply(1:10,function(x) rnorm(1000))
 colnames(X) <- paste0('x',1:10)
-Y[,1] <- Y[,3] + Y[,5] + X[,2] + X[,3]
-Y[,2] <- Y[,3] + Y[,4] + X[,5] + X[,9]
+Y[,1] <- 3 * Y[,3] + 5 * Y[,5] + 2 * X[,2] - 2 * X[,3]
+Y[,2] <- 1 * Y[,3] + 3 * X[,5] - 5 * X[,9]
 Y <- scale(Y)
 X <- scale(X)
 
