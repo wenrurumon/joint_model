f3dd2b670a985b007fcbfb54c80119f49093b9ab
進撃の雯雯老師 <askahu6@gmail.com>
2017-06-15 16:09:57 -0500

    Create macro.R

--- macro.R
+++ macro.R
@@ -1,4 +1,3 @@
-
 ############################
 # Macro
 ############################
@@ -43,32 +42,4 @@ positive <- function(x){
 dummy <- function(x,error=1){
   e <- rnorm(length(x),0,error*sd(x))
   x + e
-}
-
-#Calculation in model
-
-calc_d <- function(wi,x,yi,rho=0,Z=0,U=0){
-  if(sum(abs(Z),abs(U))==0){
-    Z <- 0
-  } else {
-    Z <- Z-U
-  }
-  out <- ginv(t(wi)%*%x%*%ginv(t(x)%*%x)%*%t(x)%*%wi+rho*diag(ncol(wi))) %*%
-    (t(wi)%*%x%*%ginv(t(x)%*%x)%*%t(x)%*%yi+rho*Z)
-  rownames(out) <- colnames(wi)
-  out
-}
-
-#equali(Yl,Xl,i,NULL,wsel,Z=z0[wsel,l],U=u0[wsel,l])
-equali <- function(Y,X,i,lambda1=0.5,wsel=NULL,Z=0,U=0){
-  yi <- Y[,i,drop=F]
-  x <- X
-  w <- cbind(Y[,-i],x)
-  if(is.null(wsel)){
-    wsel <- selbycor(yi,w,ncol(x)*lambda1)  
-  }
-  wi <- w[,wsel,drop=F]
-  di <- calc_d(wi,x,yi,rho,Z,U)
-  wsel[wsel] <- di
-  wsel
 }
