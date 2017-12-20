659cc2a73dc3f0a2055587e3a89659823beccfe2
進撃の雯雯老師 <askahu6@gmail.com>
2017-12-20 16:14:19 +0800

    Update getpathlist.R

--- get_genelist_from_kegg/getpathlist.R
+++ get_genelist_from_kegg/getpathlist.R
@@ -1,6 +1,13 @@
+
+rm(list=l())
+
 library(rvest)
+library(dplyr)
+
 url <- 'http://www.genome.jp/kegg/pathway.html'
 html <- read_html(url)
 title <- html %>% html_nodes('dd a') %>% html_text()
 href <- html %>% html_nodes('dd a') %>% html_attr('href')
-pathmap <- cbind(title=title,href=href)
+pathmap <- data.frame(title=title,href=href)
+
+#pathmap <- filter(pathmap,grepl('hsa',href))
